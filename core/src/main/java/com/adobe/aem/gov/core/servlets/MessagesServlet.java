380e2e2bf4fff3c21dbe8a9196e5d85ed5564ddc
MarilenaPaoliello <Marilenapaoliello.mp@gmail.com>
2022-05-24 21:29:28 +0200

    Add msg servlet

--- core/src/main/java/com/adobe/aem/gov/core/servlets/MessagesServlet.java
+++ core/src/main/java/com/adobe/aem/gov/core/servlets/MessagesServlet.java
@@ -1,3 +1,4 @@
+
 package com.adobe.aem.gov.core.servlets;
 
 
@@ -36,98 +37,106 @@ import java.sql.Connection;
 import java.sql.Date;
 
 /**
- * Servlet that writes some sample content into the response. It is mounted for
- * all resources of a specific Sling resource type. The
- * {@link SlingSafeMethodsServlet} shall be used for HTTP methods that are
- * idempotent. For write operations use the {@link SlingAllMethodsServlet}.
- */
+* Servlet that writes some sample content into the response. It is mounted for
+* all resources of a specific Sling resource type. The
+* {@link SlingSafeMethodsServlet} shall be used for HTTP methods that are
+* idempotent. For write operations use the {@link SlingAllMethodsServlet}.
+*/
 @Component(service = { Servlet.class }, 
-          property={"sling.servlet.methods=*", "sling.servlet.paths=/bin/messages"})
+property={"sling.servlet.methods=*", "sling.servlet.paths=/bin/messages"})
 
 @ServiceDescription("Get Meggases Servlet")
 
 public class MessagesServlet extends SlingAllMethodsServlet {
-    private final Logger log = LoggerFactory.getLogger(getClass());
-
-    @Reference(target = "(&(objectclass=javax.sql.DataSource)(datasource.name=localhost))")
-    private DataSource dataSource;
-
-    private static final long serialVersionUID = 1L;
-
-    @Override
-    protected void doGet(final SlingHttpServletRequest req,
-            final SlingHttpServletResponse resp) throws ServletException, IOException {
-        log.debug("### inside my Messages Servlet");
-        Connection con = getConnection();
-        List<HashMap> messages = new ArrayList<>();
-       
-        try {
-            Statement st = con.createStatement(); 
-            String query = "SELECT * FROM users ORDER BY date DESC LIMIT 10";
-            log.debug("###  Got Result Set" + query);
-            ResultSet rs = st.executeQuery(query);
-            while (rs.next()) { 
-                HashMap<String,String> singleMessage = new HashMap<>();
-                singleMessage.put("name",rs.getString("name"));
-                singleMessage.put("message",rs.getString("message"));
-                singleMessage.put("date",rs.getDate("date").toString());
-                messages.add(singleMessage);
-            }
-            
-        } catch(SQLException e) {
-        // TODO Auto-generated catch block
-        log.debug(e.getMessage());
-        }
-        Gson gson = new Gson();
-        resp.setContentType("application/json");
-        resp.getWriter().write(gson.toJson(messages));
+  private final Logger log = LoggerFactory.getLogger(getClass());
+  
+  @Reference(target = "(&(objectclass=javax.sql.DataSource)(datasource.name=localhost))")
+  private DataSource dataSource;
+  
+  private static final long serialVersionUID = 1L;
+  
+  @Override
+  protected void doGet(final SlingHttpServletRequest req,
+  final SlingHttpServletResponse resp) throws ServletException, IOException {
+    log.debug("### inside my Messages Servlet");
+    
+    List<HashMap> messages = new ArrayList<>();
+    
+    try (
+    Connection con = getConnection(); 
+    Statement st = con.createStatement(); 
+    ){
+      
+      String query = "SELECT * FROM users ORDER BY date DESC LIMIT 10";
+      log.debug("###  Got Result Set" + query);
+      ResultSet rs = st.executeQuery(query);
+      while (rs.next()) { 
+        HashMap<String,String> singleMessage = new HashMap<>();
+        singleMessage.put("name",rs.getString("name"));
+        singleMessage.put("message",rs.getString("message"));
+        singleMessage.put("date",rs.getDate("date").toString());
+        messages.add(singleMessage);
+      }
+      
+    } catch(SQLException e) {
+      // TODO Auto-generated catch block
+      log.error(e.getMessage());
     }
-
-    @Override
-    protected void doPost(final SlingHttpServletRequest req,
-            final SlingHttpServletResponse resp) throws ServletException, IOException {
-            resp.getWriter().write("### Messagio inserito con successo");
-        Connection con = getConnection();
-        String name = req.getParameter("name");
-        String message = req.getParameter("message");
-        try {
-            con.setAutoCommit(true);
-            String query = "INSERT INTO yuridb.users (name, message) VALUES (?, ?)";
-
-            PreparedStatement preparedStmt = con.prepareStatement(query);
-            preparedStmt.setString (1, name.replace("\n", "").replace("\r", "").trim());
-            preparedStmt.setString (2, message.replace("\n", "").replace("\r", "").trim()); 
-            preparedStmt.execute();
-            
-            resp.getWriter().write("###  Hai inserito per l'utente : " + name + " ### il messaggio: " + message + "\n");
-            con.close();
-          } catch(SQLException e) {
-            resp.getWriter().write(e.getMessage());
-            resp.getWriter().write("\n###  ERROR DATA : " + name + " ### " + message + "\n");
-            // TODO Auto-generated catch block
-            log.debug(e.getMessage());
-          }
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
-        resp.getWriter().write("done!");
+    Gson gson = new Gson();
+    resp.setContentType("application/json");
+    resp.getWriter().write(gson.toJson(messages));
+  }
+  
+  @Override
+  protected void doPost(final SlingHttpServletRequest req,
+  final SlingHttpServletResponse resp) throws ServletException, IOException {
+    
+    String name = req.getParameter("name");
+    String message = req.getParameter("message");
+    
+    try(Connection con = getConnection();) {
+      con.setAutoCommit(true);
+      String query = "INSERT INTO yuridb.users (name, message) VALUES (?, ?)";
+      
+      PreparedStatement preparedStmt = con.prepareStatement(query);
+      preparedStmt.setString (1, name.replace("\n", "").replace("\r", "").trim());
+      preparedStmt.setString (2, message.replace("\n", "").replace("\r", "").trim()); 
+      preparedStmt.execute();
+      
+      
+      resp.getWriter().write("### Messagio inserito con successo!  Hai inserito per l'utente : " + name + " ### il messaggio: " + message + "\n");
+      
+      
+    } catch(SQLException e) {
+      resp.getWriter().write(e.getMessage());
+      resp.getWriter().write("\n###  ERROR DATA : " + name + " ### " + message + "\n");
+      // TODO Auto-generated catch block
+      log.error(e.getMessage());
+    } 
+    
+    resp.setContentType("application/json");
+    resp.setCharacterEncoding("UTF-8");
+    resp.getWriter().write("done!");
+    
+    
+  }
+  
+  
+  public Connection getConnection() {
+    log.debug("### Getting Connection ");
+    Connection con = null;
+    try {
+      
+      con = dataSource.getConnection();
+      log.debug("### got connection");
+      return con;
+    } catch(Exception e) {
+      log.error("### not able to get connection " + e.getMessage());
+      
     }
-
-
-    public Connection getConnection() {
-        log.debug("### Getting Connection ");
-        Connection con = null;
-        try {
-
-          con = dataSource.getConnection();
-          log.debug("### got connection");
-          return con;
-        } catch(Exception e) {
-          log.debug("### not able to get connection " + e.getMessage());
-
-        }
-        return null;
-    }
-
-
-
+    return null;
+  }
+  
+  
+  
 }
