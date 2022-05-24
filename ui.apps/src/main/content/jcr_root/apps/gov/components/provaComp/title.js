d93246e5d4798c9ac74c7532d0320b17a110ebc6
MarilenaPaoliello <Marilenapaoliello.mp@gmail.com>
2022-05-24 21:30:41 +0200

    Add prova comp

---
+++ ui.apps/src/main/content/jcr_root/apps/gov/components/provaComp/title.js
@@ -0,0 +1,29 @@
+/* use( function(){
+    console.log("messaggio js stampato");
+    let title = {
+        text:"prova Alex",
+        number: 12,
+        nestedObj:{
+            key1:"val1", 
+            key2:"val2", 
+            key3:"val3",
+            key4:"val4"
+
+        }
+
+    }
+    return title;
+}
+
+); */
+$( document ).ready(function titleFunc() {
+    $("#buttonM").click(function(){
+        console.log("sto caricando il js");
+        
+    })
+});
+/* document.addEventListener("DOMContentLoaded", function titleFunc() {
+    document.querySelector("#buttonM").addEventListener("click", function() {
+        console.log("ifjr");
+    })
+}) */
